6ed956fbbda1a1dfac9e5da4aa0aa11d5304157a
AWLR <149447442+AWLR@users.noreply.github.com>
2024-01-28 12:52:13 +0800

    added comments

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 a = input("Enter a string:")
-print(len(a))
-print(a.upper())
-print(a.lower())
-print(a[0])
-print(a[-1])
+print(len(a)) #length of string
+print(a.upper()) #upper case
+print(a.lower()) #lower case
+print(a[0]) #first letter
+print(a[-1]) #last letter
